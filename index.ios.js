e5d4b53cdc9c42b35ba322dc67a08a6bc27def14
bcsasankh <bcsasankh@gmail.com>
2017-03-24 20:23:07 -0700

    added style

--- index.ios.js
+++ index.ios.js
@@ -4,18 +4,29 @@ var ReactNative = require('react-native');
 var AppRegistry = ReactNative.AppRegistry;
 var Text = ReactNative.Text;
 var View = ReactNative.View;
+var StyleSheet = ReactNative.StyleSheet;
 
 // Create a react Component
 var Weekdays = React.createClass({
   render: () => {
-    return <View>
-      <Text>
-        Days of the week:
-      </Text>
-    </View>
+    return (
+      <View style={styles.container}>
+        <Text>
+          Days of the week:
+        </Text>
+      </View>
+    );
   }
 });
 
+//Style the React Component
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 // Show the react component on the screen
 AppRegistry.registerComponent('weekdays', () => {
